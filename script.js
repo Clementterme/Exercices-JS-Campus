5af81197e64b91418712c85b68909e9163830f6a
Clementterme <clement.terme.simplon@gmail.com>
2026-02-17 11:36:44 +0100

    Formulaire dynamique fonctionnel

--- script.js
+++ script.js
@@ -4,3 +4,24 @@ function deroulerMenu() {
     menu.classList.toggle("cacher");
     menu.classList.toggle("afficher");
 }
+
+const form = document.getElementById("form");
+
+form.addEventListener("submit", function(event) {
+    event.preventDefault();
+});
+
+function afficherMessage() {
+    let message = document.querySelector("#texte").value;
+
+    let nouvelleDiv = document.createElement("div");
+
+    let contenu = document.createTextNode(message);
+
+    nouvelleDiv.appendChild(contenu);
+
+    form.after(nouvelleDiv);
+
+    let zoneTexte = document.querySelector("#texte");
+    zoneTexte.value = "";
+}
